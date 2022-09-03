0eec72487fbf304bae52b9727f2225d357d11fb9
Andrew1929 <108069918+Andrew1929@users.noreply.github.com>
2022-09-03 22:09:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace StreamReadWrite
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = int.Parse("475");
+            Console.WriteLine(k);
+
+
+        }
+    }
+}
